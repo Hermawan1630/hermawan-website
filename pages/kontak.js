0c27145bf4931efd248cc5aa6fe2cf8dfc5d8a92
Hermawan Tri Santoso <hermawantrisantoso96@gmail.com>
2025-06-11 21:35:16 +0700

    update teks di kontak

--- pages/kontak.js
+++ pages/kontak.js
@@ -1,9 +1,15 @@
 export default function Kontak() {
   return (
     <main className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Kontak Saya</h1>
-      <p className="text-gray-700">Hubungi saya di email: hermawantrisantoso96@gmail.com
-      </p>
+        <section className="max-w-3xl mx-auto bg-white rounded-lg p-6 shadow-md">
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Kontak Saya</h1>
+            <p className="text-gray-700">Hubungi saya melalui
+            <p className="text-gray-700">email: tteokboki_jae_malang@gmail.com</p>
+            <p className="text-gray-700">nomor WA: 085678333212</p>
+            <p className="text-gray-700">IG: @tteok_jae_malang</p>
+            <p className="text-gray-700">Tiktok: @tteok_jae_malang</p>
+            </p>
+        </section>
     </main>
   );
 }
